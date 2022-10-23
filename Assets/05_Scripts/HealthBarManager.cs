eab49cd01f914ef83d30e73d60bedd2bfb720908
HappyMirandum <u37773@hs-harz.de>
2022-10-23 19:03:11 +0200

    HealthBarTester Finale Version (1)

--- Assets/05_Scripts/HealthBarManager.cs
+++ Assets/05_Scripts/HealthBarManager.cs
@@ -11,43 +11,53 @@ namespace _05_Scripts
     
         [SerializeField] private TMP_InputField tankAttackInputField;
         [SerializeField] private TMP_InputField crossbowAttackInputField;
-        [SerializeField] private TMP_InputField poisonAttackInputField;
         [SerializeField] private TMP_InputField bombAttackInputField;
-        [SerializeField] private TMP_InputField shielAttackInputField;
         [SerializeField] private TMP_InputField medikitAttackInputField;
 
         [SerializeField] private Slider healthBar;
 
         [SerializeField] private ParticleSystem panzerEffect;
         [SerializeField] private ParticleSystem crossbowEffect;
-        [SerializeField] private ParticleSystem poisonEffect;
         [SerializeField] private ParticleSystem bombEffect;
-        [SerializeField] private ParticleSystem shieldEffect;
         [SerializeField] private ParticleSystem medikitEffect;
 
+        [SerializeField] private GameObject[] charSkin;
     
         //Identifiers beginning with an underscore
         private float _tankAttackAmount;
         private float _crossbowAttackAmount;
-        private float _poisonAttackAmount;
         private float _bombAttackAmount;
-        private float _shieldAttackAmount;
         private float _medikitAttackAmount;
     
-        // Identifiers beginning with an underscore followed immediately by an uppercase letter
-        public float Test = 0.1f;
+        private int _randomCharNumber, _activeIndex;
 
+        // Identifiers beginning with an underscore followed immediately by an uppercase letter
         // _ (underscore) = avoid collision of names
+        
 
         #endregion
-    
-    
+        
+        
+        public void RerollChar()
+        {
+            do
+            {
+                _randomCharNumber = Random.Range(0, 3);
+            } while (charSkin[_activeIndex] == charSkin[_randomCharNumber]);
+
+            charSkin[_activeIndex].SetActive(false);
+            charSkin[_randomCharNumber].SetActive(true);
+
+            _activeIndex = _randomCharNumber;
+        }
+
+
         public void TankTest()
         {
             float.TryParse(tankAttackInputField.text, out _tankAttackAmount);
 
             // if not (_tankAttackAmount > 0 = true) do nothing.
-            if (!(_tankAttackAmount > 0)) return;
+            if (_tankAttackAmount <= 0 || healthBar.value == healthBar.minValue) return;
             healthBar.value -= _tankAttackAmount;
             panzerEffect.Play();
         }
@@ -56,43 +66,31 @@ namespace _05_Scripts
         public void CrossbowTest()
         {
             float.TryParse(crossbowAttackInputField.text, out _crossbowAttackAmount);
-            if (!(_crossbowAttackAmount > 0)) return;
+            if (_crossbowAttackAmount <= 0 || healthBar.value == healthBar.minValue) return;
             healthBar.value -= _crossbowAttackAmount;
             crossbowEffect.Play();
-        }
+        } 
+    
 
-    
-        public void PoisonTest()
-        {
-            //TODO: Create PoisonTest function
-        }
-    
-    
         public void BombTest()
         {
             float.TryParse(bombAttackInputField.text, out _bombAttackAmount);
-            if (!(_bombAttackAmount > 0)) return;
+            if (!(_bombAttackAmount > 0) || healthBar.value == healthBar.minValue) return;
             healthBar.value -= _bombAttackAmount;
             bombEffect.Play();
         }
+    
 
-    
-        public void ShieldTest()
-        {
-            //TODO: Create ShieldTest function
-        }
-    
-    
         public void MedikitTest()
         {
             float.TryParse(medikitAttackInputField.text, out _medikitAttackAmount);
 
-            if (!(_medikitAttackAmount > 0)) return;
+            if (!(_medikitAttackAmount > 0) || healthBar.value == healthBar.maxValue) return;
             healthBar.value += _medikitAttackAmount;
             medikitEffect.Play();
-        }
+        } 
     
-    
+
         public void QuitOnClick()
         {
             Debug.Log("ButtonClick - Quit");
--- Assets/05_Scripts/RotateObj.cs
+++ Assets/05_Scripts/RotateObj.cs
@@ -7,11 +7,15 @@ namespace _05_Scripts
     {
         [Header("Accelerometer")]
         [Tooltip("(float accelerometerY * Time.deltaTime)")]
+        [SerializeField] private float accelerometerX;
         [SerializeField] private float accelerometerY;
-        
+        [SerializeField] private float accelerometerZ;
+
         void Update()
         {
-            transform.Rotate (0, accelerometerY * Time.deltaTime, 0);
+            transform.Rotate(0, accelerometerY * Time.deltaTime, 0);
+            transform.Rotate(accelerometerX * Time.deltaTime, 0, 0);
+            transform.Rotate(0, 0, accelerometerZ * Time.deltaTime);
         }
     }
 }
